c765ce2ba22e4ecb9a2c3ea0d392117bf5d91e89
Nims09 <nmots@live.ca>
2013-07-03 22:33:03 -0700

    Added a main and some doc

---
+++ ThisSongSucks.java
@@ -0,0 +1,17 @@
+/**
+ * This is the main for ThisSongSucksApp
+ * 
+ * Call by: java ThisSongSucks.java
+ * 
+ * @author NMots
+ * 
+ */
+
+
+public class ThisSongSucks {
+    public static void main (String[] args) {
+        for (String s: args) {
+      	  System.out.println(s);
+        }
+    }
+}
