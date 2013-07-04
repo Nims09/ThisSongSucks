96f8e630bd7ca8e4946f12502a54496b0c31aca7
Nims09 <nmots@live.ca>
2013-07-03 23:05:52 -0700

    added some finals to the main

--- ThisSongSucks.java
+++ ThisSongSucks.java
@@ -1,7 +1,11 @@
+import java.lang.reflect.Array;
+
 /**
  * This is the main for ThisSongSucksApp
  * 
- * Call by: java ThisSongSucks.java
+ * Call by: java ThisSongSucks.java --<option>
+ * 
+ * call 'java ThisSongSucks.java --help' for help
  * 
  * @author NMots
  * 
@@ -9,9 +13,26 @@
 
 
 public class ThisSongSucks {
-    public static void main (String[] args) {
-        for (String s: args) {
-      	  System.out.println(s);
-        }
-    }
+	
+	final String ABOUT = "about";
+	final String PREFRENCES = "prefrences";
+	final String TRACK_ADD = "trackadd";
+	final String TRACK_ADD_ABV = "ta";
+	final String TRACK_REMOVE = "trackremove";
+	final String TRACK_REMOVE_ABV = "tr";
+	final String RUN_REVIEW = "runreview";
+	final String RUN_REVIEW_ABV = "rr";
+	
+	final int OPTIONS_SIZE = 6;
+	//Best form of a table ?? 
+	//final HashMap() PARAM_OPTIONS = new HashMap();
+	
+	
+	
+	public static void main (String[] args) {
+		// Parse command line 
+		for (String a: args) {
+      	  System.out.println(a);
+		}
+	}
 }
