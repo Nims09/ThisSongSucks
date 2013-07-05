87049d0293df7023471c7035ee74ad98cbcc82d6
Nims09 <nmots@live.ca>
2013-07-04 22:59:29 -0700

    filled in some skeleton for initial conditions

--- ThisSongSucks.java
+++ ThisSongSucks.java
@@ -1,4 +1,7 @@
 import java.lang.reflect.Array;
+import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * This is the main for ThisSongSucksApp
@@ -14,25 +17,107 @@ import java.lang.reflect.Array;
 
 public class ThisSongSucks {
 	
-	final String ABOUT = "about";
-	final String PREFRENCES = "prefrences";
-	final String TRACK_ADD = "trackadd";
-	final String TRACK_ADD_ABV = "ta";
-	final String TRACK_REMOVE = "trackremove";
-	final String TRACK_REMOVE_ABV = "tr";
-	final String RUN_REVIEW = "runreview";
-	final String RUN_REVIEW_ABV = "rr";
-	
-	final int OPTIONS_SIZE = 6;
-	//Best form of a table ?? 
-	//final HashMap() PARAM_OPTIONS = new HashMap();
-	
-	
+	final static String HELP = "help";
+	final static String ABOUT = "about";
+	final static String PREFRENCES = "prefrences";
+	final static String TRACK_ADD = "trackadd";
+	final static String TRACK_ADD_ABV = "ta";
+	final static String TRACK_REMOVE = "trackremove";
+	final static String TRACK_REMOVE_ABV = "tr";
+	final static String RUN_REVIEW = "runreview";
+	final static String RUN_REVIEW_ABV = "rr";
 	
 	public static void main (String[] args) {
-		// Parse command line 
-		for (String a: args) {
-      	  System.out.println(a);
+		try {
+			// Backwards load stack of parameters, then pop the first one
+			Stack<String> paramStack = new Stack<String>(); 			
+			for(int i = args.length; i != 0; i--)
+			{
+				paramStack.push(args[i]);
+			}
+			String initialParameter = paramStack.pop();
+			
+
+			if(initialParameter.equals(ABOUT))
+			{
+				callAbout();
+			}
+			else if(initialParameter.equals(PREFRENCES))
+			{
+				callPrefrences();
+			}
+			else if(initialParameter.equals(TRACK_ADD) || initialParameter.equals(TRACK_ADD_ABV))
+			{
+				callTrackAdd();
+			}
+			else if(initialParameter.equals(TRACK_REMOVE) || initialParameter.equals(TRACK_REMOVE_ABV))
+			{
+				callTrackRemove();
+			}
+			else if(initialParameter.equals(RUN_REVIEW) || initialParameter.equals(RUN_REVIEW_ABV))
+			{
+				callRunReview();
+			}
+			else if(initialParameter.equals(HELP))
+			{
+				callHelp();
+			}
+			else
+			{
+				callHelp();
+			}
+		
+		} catch (Exception e) {
+		
 		}
+			
+	}
+	
+	/**
+	 * 
+	 */
+	static void callAbout()
+	{
+		System.out.println(ABOUT);
+	}
+	
+	/**
+	 * 
+	 */
+	static void callPrefrences()
+	{
+		System.out.println(PREFRENCES);
+	}
+	
+	/**
+	 * 
+	 */
+	static void callTrackAdd()
+	{
+		System.out.println(TRACK_ADD);
+	}
+	
+	/**
+	 * 
+	 */
+	static void callTrackRemove()
+	{
+		System.out.println(TRACK_REMOVE);
+	}
+	
+	/**
+	 * 
+	 */
+	static void callRunReview()
+	{
+		System.out.println(RUN_REVIEW);
+	}
+	
+	/**
+	 * 
+	 */
+	static void callHelp()
+	{
+		System.out.println(HELP);
 	}
 }
