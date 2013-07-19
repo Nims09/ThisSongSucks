775be65f51ea72cb5860fe28e7ad962c820f9d71
Nims09 <nmots@live.ca>
2013-07-18 22:48:28 -0700

    Fixed final build statments and started verbose setting

--- glassBlower.sh
+++ glassBlower.sh
@@ -12,9 +12,18 @@ dirList=(
     'Interfaces'
     'Utilities'
 )
+
 # Create String variables
 buildString='javac '
 jarString='jar '
+
+# Check if we want to run verbose
+verbose=0
+
+if [ $# = "-v" ]; then #FIX ME THIS NEEDS A PROPER CHECK 
+    verbose=1
+    echo "Verbose turned on. "
+fi
 
 # Build Directories
 buildString="$buildString *.java"
@@ -22,9 +31,41 @@ buildString="$buildString *.java"
 for i in "${dirList[@]}"
 do
     buildString="$buildString $i/*.java"
-    echo $buildString
+
+    if [ verbose = 1 ]; then
+        echo "Added directory: $i to build string. "
+    fi
+done
+
+# Collect class files and Jar them
+jarString="$jarString cfe ThisSongSucks.jar ThisSongSucks ThisSongSucks.class "
+jarString="$jarString *.class"
+
+for i in "${dirList[@]}"
+do
+    jarString="$jarString $i/"
+
+    if [ verbose = 1 ]; then
+        echo "Added directory: $i to jar string. "
+    fi
 done
 
 # Run the build command
+if [ verbose = 1 ]; then
+    echo "Running build command: "
+    echo $buildString
+fi
 $buildString
+
+# Run our jar command
+if [ verbose = 1 ]; then
+    echo "Running jar command: "
+    echo $jarString
+fi
+$jarString
+
+# Script complete
+if [ verbose = 1 ]; then
+    echo "Script complete. "
+fi
 
