d8d59820ef353fd77001470b8c1778ed6fed0998
Nims09 <nmots@live.ca>
2013-07-24 22:49:11 -0700

    got a custom file working

--- DB/dbBuilder.java
+++ DB/dbBuilder.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 public class dbBuilder
 {
 	// Add finals
-	public static final String DB_CONNECTION = "jbdc.sqlite:database.db";
+	public static final String DB_CONNECTION = "jdbc.sqlite:database.db");
 	public static final int CONNECTION_TIMEOUT_VALUE = 30;
 	
 	/**
@@ -16,7 +16,7 @@ public class dbBuilder
 	 * Should only be run if tables 
 	 * don't already exist
 	 */
-	public void createTables() throws ClassNotFoundException
+	public static void createTables() throws ClassNotFoundException
 	{
 		// Load the sqlite-JDBC driver for the current class loader
 		Class.forName("org.sqlite.JDBC");
@@ -29,10 +29,41 @@ public class dbBuilder
 	      Statement statement = connection.createStatement();
 	      statement.setQueryTimeout(CONNECTION_TIMEOUT_VALUE);  // set timeout to 30 sec.
 			
+	      statement.executeUpdate("drop table if exists person");
+	      statement.executeUpdate("create table person (id integer, name string)");
+	      statement.executeUpdate("insert into person values(1, 'leo')");
+	      statement.executeUpdate("insert into person values(2, 'yui')");
+
+	      ResultSet rs = statement.executeQuery("select * from person");
+	      while(rs.next())
+	      {
+	        // read the result set
+	        System.out.println("name = " + rs.getString("name"));
+	        System.out.println("id = " + rs.getInt("id"));
+	      }
 		}
 		catch(Exception e)
 		{
 			// Do somthing
+			System.out.println(e.getMessage());
 		}
+		  finally
+		    {
+		      try
+		      {
+		        if(connection != null)
+		          connection.close();
+		      }
+		      catch(SQLException e)
+		      {
+		        // connection close failed.
+		        System.err.println(e);
+		      }
+		    }
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		createTables();
 	}
 }
