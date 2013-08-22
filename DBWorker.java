a00ebb78f39d117229083c5860d69a4b2d222fe6
Nims09 <nmots@live.ca>
2013-08-21 22:20:11 -0700

    added DB worker and gave it a constructor

---
+++ DBWorker.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+
+public class DBWorker 
+{
+	// Add globals
+	protected Connection connection;
+	
+	/**
+	 * Constructor
+	 */
+	public DBWorker (Connection connection)
+	{
+		this.connection = connection 
+	}
+		
+	
+}
